2f720b4c756e5875b3ff670776cd3da81ff51b35
aaronmesupe <aaronmp1990@gmail.com>
2017-07-14 10:14:54 +0200

    He conseguido ocultar los elementos LI de los precios y que el precio sea 13 por defecto. Solo nos queda sumar

--- src/pizza.js
+++ src/pizza.js
@@ -1,11 +1,16 @@
 // Write your Pizza Builder JavaScript in this file.
 window.onload = function() {
 event.preventDefault();
+var liPrice = $('.price').find('li');
 
 $('.btn-crust').toggleClass('active');
 $('.btn-sauce').toggleClass('active');
-$('.sauce').toggleClass('sauce-white');	
+$('.sauce').toggleClass('sauce-white');
+$(liPrice[3]).hide();
 $('.crust').toggleClass('crust-gluten-free');
+$(liPrice[4]).hide();
+$('strong').text('$13')
+
 
 /*if( $('section').hasClass('crust-gluten-free') === true) {
 	$('section').removeClass('crust-gluten-free');
@@ -23,26 +28,32 @@ if( $('button').hasClass('btn-sauce') === true) {
 
 
 
+
 $('.btn-pepperonni').click(function(){
 		$('.pep').fadeToggle();
+		$(liPrice[0]).fadeToggle();
 });
 
 $('.btn-mushrooms').click(function(){
 		$('.mushroom').fadeToggle();
+		$(liPrice[1]).fadeToggle();
 });
 
 $('.btn-green-peppers').click(function(){
 		$('.green-pepper').fadeToggle();
+		$(liPrice[2]).fadeToggle();
 });
 
 $('.btn-sauce').click(function(){
 		$('.sauce').toggleClass('sauce-white');
 		$('.btn-sauce').toggleClass('active');
+		$(liPrice[3]).fadeToggle();
 });
 
 $('.btn-crust').click(function(){
 		$('.crust').toggleClass('crust-gluten-free');
 		$('.btn-crust').toggleClass('active');
+		$(liPrice[4]).fadeToggle();
 });
 
 
